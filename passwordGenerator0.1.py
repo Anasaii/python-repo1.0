57e5954a270da3ca57355f4c27c5b3e936d29086
Anasaii <>
2022-08-26 23:06:07 -0400

    functional build

--- passwordGenerator0.1.py
+++ passwordGenerator0.1.py
@@ -1,40 +1,45 @@
+import random
+import pyperclip
 from curses.ascii import isdigit
 from re import X
 import secrets, string, msvcrt
 
 """
 TODO:
-upperMin
-lowerMin
-digitMin
-charcMin
+upperMin = -u
+lowerMin = -l
+digitMin = -d
+charcMin = -c
 """
-password = ""
 
-def formulize(): 
-    password += secrets.choice(string.ascii_lowercase)
-    password += secrets.choice(string.ascii_uppercase)
-    password += secrets.choice(string.punctuation)
-    password += secrets.choice(string.digits)
+
+def formulize(a): 
+    password = []
+    finalPassword = ""
+
+    for x in range(a):
+        password.append(secrets.choice([secrets.choice(string.ascii_letters), secrets.choice(string.punctuation), secrets.choice(string.digits)]))
+        print(password)
+
+    random.shuffle(password)
+    finalPassword = ''.join(map(str, password))
+    return finalPassword
 
 while 0 < 1:
     print("Type length of password (0-9) ")
     print(60 * '-')
-    try: 
-        session_id = int(msvcrt.getch().decode('ASCII'))
+    session_id = int(msvcrt.getch().decode('ASCII'))
 
-        if isinstance(session_id, int) == True:
-            #formulize(session_id)
-            print(password)
-            break
-    
-        elif session_id :
-            print("Input not from 0-9. Try again")
+    if isinstance(session_id, int) == True:
+        finalPassword = (formulize(session_id))
+        print(finalPassword)
+        pyperclip.copy(finalPassword)
+        break
+        
+    else: 
+        print('You are stupid. Use a number')
 
-    except:
+
+    #except:
         print('You are stupid. Use a number')
         
-
-
-
-print(string.digits)
